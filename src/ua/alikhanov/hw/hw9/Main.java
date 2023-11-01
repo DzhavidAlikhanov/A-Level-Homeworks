725d7f4f6286929ba5515cd28dd6fff589f1622a
DzhavidAlikhanov <146656989+DzhavidAlikhanov@users.noreply.github.com>
2023-11-01 18:42:54 +0200

    hw 9

---
+++ src/ua/alikhanov/hw/hw9/Main.java
@@ -0,0 +1,26 @@
+package ua.alikhanov.hw.hw9;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal("Лев", 10, "мясо");
+        Animal animal2 = new Animal("Слон", 14, "траву");
+        Animal animal3 = new Animal("Тигр", 12, "мясо");
+
+        Animal maxSleepingAnimal = animal1;
+        if (animal2.getHoursOfSleeping() > maxSleepingAnimal.getHoursOfSleeping()) {
+            maxSleepingAnimal = animal2;
+        }
+        if (animal3.getHoursOfSleeping() > maxSleepingAnimal.getHoursOfSleeping()) {
+            maxSleepingAnimal = animal3;
+        }
+
+        System.out.println(maxSleepingAnimal.getName() + " спит больше всех.");
+        System.out.println(maxSleepingAnimal.getName() + " ест " + maxSleepingAnimal.getFood() + ".");
+
+        Receiver receiver = new Receiver(42, "строка");
+
+        receiver.method1();
+        receiver.method2();
+        receiver.method3();
+    }
+}
---
+++ src/ua/alikhanov/hw/hw9/Receiver.java
@@ -0,0 +1,23 @@
+package ua.alikhanov.hw.hw9;
+
+public class Receiver {
+    private int variable1;
+    private String variable2;
+
+    public Receiver(int variable1, String variable2) {
+        this.variable1 = variable1;
+        this.variable2 = variable2;
+    }
+
+    public void method1() {
+        System.out.println("Метод 1 выполнен.");
+    }
+
+    public void method2() {
+        System.out.println("Метод 2 выполнен.");
+    }
+
+    public final void method3() {
+        System.out.println("Метод 3 выполнен (final).");
+    }
+}
