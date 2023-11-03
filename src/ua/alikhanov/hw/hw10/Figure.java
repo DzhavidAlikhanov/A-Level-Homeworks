2a2641f4439c60f15493a2048b060e021ef7d1ab
DzhavidAlikhanov <146656989+DzhavidAlikhanov@users.noreply.github.com>
2023-11-03 18:54:30 +0200

    hw 10

---
+++ src/ua/alikhanov/hw/hw10/Figure.java
@@ -0,0 +1,10 @@
+package ua.alikhanov.hw.hw10;
+
+public class Figure {
+    public double perimeter(){
+        return 0.0;
+    }
+    public double area() {
+        return 0.0;
+    }
+}
---
+++ src/ua/alikhanov/hw/hw10/Main.java
@@ -0,0 +1,30 @@
+package ua.alikhanov.hw.hw10;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Square square = new Square(4.0);
+        Triangle triangle = new Triangle(3.0, 4.0, 5.0, 6.0, 4.0, 41.0);
+
+        double circlePerimeter = circle.perimeter();
+        double circleArea = circle.area();
+
+        double squarePerimeter = square.perimeter();
+        double squareArea = square.area();
+
+        double triangleHeronArea = triangle.areaHeron();
+        double triangleSineArea = triangle.areaSine();
+        double triangleBaseHeihtArea = triangle.areaBaseHeight();
+
+        System.out.println("Circle Perimeter: " + circlePerimeter);
+        System.out.println("Circle Area: " + circleArea);
+        System.out.println(" ");
+        System.out.println("Square Perimeter: " + squarePerimeter);
+        System.out.println("Square Area: " + squareArea);
+        System.out.println(" ");
+        System.out.println("Triangle Heron Area: " + triangleHeronArea);
+        System.out.println("Triangle Sine Area: " + triangleSineArea);
+        System.out.println("Triangle Base Height Area: " + triangleBaseHeihtArea);
+
+    }
+}
