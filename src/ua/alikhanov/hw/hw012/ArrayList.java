0b981a35f5a8e2c6ed0292ad68ce51c226a8c185
DzhavidAlikhanov <146656989+DzhavidAlikhanov@users.noreply.github.com>
2023-11-17 15:07:38 +0200

    hw012 add

--- src/ua/alikhanov/hw/hw012/ArrayList.java
+++ src/ua/alikhanov/hw/hw012/ArrayList.java
@@ -1,22 +1,20 @@
 package ua.alikhanov.hw.hw012;
 
 public class ArrayList<T> {
-    protected Object[] array;
-    protected int size;
+    private Object[] array;
+    int size;
 
-    ArrayList(int size) {
-        this.size = size;
-        array = new Object[size];
+    public ArrayList(int size) {
+        this.size = 0;
+        array = new Object[Math.max(size, 10)];
     }
 
-    //Add element to the end of array {1,2,3} -> {1,2,3,4}
-    void add(T element) {
+    public void add(T element) {
         ensureCapacity();
         array[size++] = element;
     }
 
-    //Add element to given position of array {1,2,3} -> {1,4,2,3}
-    void add(T element, int index) {
+    public void add(T element, int index) {
         ensureCapacity();
         if (index >= 0 && index <= size) {
             System.arraycopy(array, index, array, index + 1, size - index);
@@ -27,8 +25,7 @@ public class ArrayList<T> {
         }
     }
 
-    // Returns index of the given element, if element doesn't exist return -1
-    int indexOf(T element) {
+    public int indexOf(T element) {
         for (int i = 0; i < size; i++) {
             if (element.equals(array[i])) {
                 return i;
@@ -37,11 +34,11 @@ public class ArrayList<T> {
         return -1;
     }
 
-    boolean isContains(T element) {
+    public boolean isContains(T element) {
         return indexOf(element) != -1;
     }
 
-    T get(int index) {
+    public T get(int index) {
         if (index >= 0 && index < size) {
             return (T) array[index];
         } else {
@@ -49,18 +46,18 @@ public class ArrayList<T> {
         }
     }
 
-    T remove(int index) {
+    public T remove(int index) {
         if (index >= 0 && index < size) {
             T removedElement = (T) array[index];
             System.arraycopy(array, index + 1, array, index, size - index - 1);
-            array[--size] = null; // to avoid memory leak
+            array[--size] = null;
             return removedElement;
         } else {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
     }
 
-    private void ensureCapacity() {
+    protected void ensureCapacity() {
         if (size == array.length) {
             int newCapacity = size * 2;
             array = java.util.Arrays.copyOf(array, newCapacity);
